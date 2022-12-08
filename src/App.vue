a03098712ad14fd6796817ec3d86f1dbfc717f7f
xinxu <xin872906473@163.com>
2022-12-08 10:24:07 +0800

    init template first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="mb-4">
+    <SFCButton color="blue" rounded plain>主要按钮</SFCButton>
+    <SFCButton color="green" rounded plain>绿色按钮</SFCButton>
+    <SFCButton color="gray" rounded plain>灰色按钮</SFCButton>
+    <SFCButton color="yellow" rounded plain>黄色按钮</SFCButton>
+    <SFCButton color="red" rounded plain>红色按钮</SFCButton>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import ZiYuUI from "ziyu-vite-ui";
+import "ziyu-vite-ui/dist/style.css";
+
+createApp(App).use(ZiYuUI).mount("#app");
